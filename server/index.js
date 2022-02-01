ab67fd1d15dbc3e675bd17bb56fe92b0b9101940
baraaAlosaily <seelbaraa@gmail.com>
2022-02-01 17:20:17 +0200

    e-commerce backend

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const cors=require("cors");
+
+const userRoute=require("./routes/user");
+const authRoute=require("./routes/auth");
+const productRoute=require("./routes/product");
+const cartRoute=require("./routes/cart");
+const orderRoute=require("./routes/order");
+const stripeRoute=require("./routes/stripe");
+
+dotenv.config();
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology: true})
+.then(()=>{
+    console.log("DB is working well");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
+app.use("/api/orders",orderRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/checkout",stripeRoute);
+
+
+app.listen(process.env.PORT||5000,()=>{
+    console.log("Backend server is running");
+});
